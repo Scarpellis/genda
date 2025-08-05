2735cfd4d2428bc5358c3acf1f8a1f19a186f78d
Scarpellis <127776273+Scarpellis@users.noreply.github.com>
2025-08-05 14:50:16 -0300

    feat: add users listing endpoint

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('../controllers/usersController');
+const { createUser, getUsers } = require('../controllers/usersController');
 
+router.get('/', getUsers);
 router.post('/', createUser);
 
 module.exports = router;
