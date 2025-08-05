9f45a29fb09157a8e023d4d9c6fa39a37f7dfc7f
Scarpellis <127776273+Scarpellis@users.noreply.github.com>
2025-08-05 15:38:00 -0300

    Revert "refactor: move user creation and listing to service"

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, listUsers } = require('../controllers/usersController');
+const { createUser } = require('../controllers/usersController');
 
 router.post('/', createUser);
-router.get('/', listUsers);
 
 module.exports = router;
